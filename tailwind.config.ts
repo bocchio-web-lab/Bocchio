412e83eb568106e95bddf66780d858839869eefb
Bocchio01 <tommaso.bocchietti@gmail.com>
2024-09-06 22:16:09 +0200

    Initial commit

---
+++ tailwind.config.ts
@@ -0,0 +1,81 @@
+import { fontFamily } from "tailwindcss/defaultTheme";
+import type { Config } from "tailwindcss";
+
+const config: Config = {
+	darkMode: ["class"],
+	content: ["./src/**/*.{html,js,svelte,ts}"],
+	safelist: ["dark"],
+	theme: {
+		container: {
+			center: true,
+			padding: "2rem",
+			screens: {
+				"2xl": "1400px"
+			}
+		},
+		extend: {
+			colors: {
+				border: "hsl(var(--border) / <alpha-value>)",
+				input: "hsl(var(--input) / <alpha-value>)",
+				ring: "hsl(var(--ring) / <alpha-value>)",
+				background: "hsl(var(--background) / <alpha-value>)",
+				foreground: "hsl(var(--foreground) / <alpha-value>)",
+				primary: {
+					DEFAULT: "hsl(var(--primary) / <alpha-value>)",
+					foreground: "hsl(var(--primary-foreground) / <alpha-value>)"
+				},
+				secondary: {
+					DEFAULT: "hsl(var(--secondary) / <alpha-value>)",
+					foreground: "hsl(var(--secondary-foreground) / <alpha-value>)"
+				},
+				destructive: {
+					DEFAULT: "hsl(var(--destructive) / <alpha-value>)",
+					foreground: "hsl(var(--destructive-foreground) / <alpha-value>)"
+				},
+				muted: {
+					DEFAULT: "hsl(var(--muted) / <alpha-value>)",
+					foreground: "hsl(var(--muted-foreground) / <alpha-value>)"
+				},
+				accent: {
+					DEFAULT: "hsl(var(--accent) / <alpha-value>)",
+					foreground: "hsl(var(--accent-foreground) / <alpha-value>)"
+				},
+				popover: {
+					DEFAULT: "hsl(var(--popover) / <alpha-value>)",
+					foreground: "hsl(var(--popover-foreground) / <alpha-value>)"
+				},
+				card: {
+					DEFAULT: "hsl(var(--card) / <alpha-value>)",
+					foreground: "hsl(var(--card-foreground) / <alpha-value>)"
+				}
+			},
+			borderRadius: {
+				lg: "var(--radius)",
+				md: "calc(var(--radius) - 2px)",
+				sm: "calc(var(--radius) - 4px)"
+			},
+			fontFamily: {
+				sans: [...fontFamily.sans],
+                heading: ['Catamaran', 'sans-serif'],
+                // body: ['Roboto Condensed', 'sans-serif'],
+            },
+            keyframes: {
+                'sloganReveal': {
+                    '0%': { opacity: '0', 'text-shadow': 'none' },
+                    '70%': { opacity: '1', 'text-shadow': 'none' },
+                    '100%': { opacity: '1', 'text-shadow': ' -5px 5px 5px hsla(0, 0%, 100%, .5)' }
+                },
+                'particleReveal': {
+                    '0%': { opacity: '0' },
+                    '100%': { opacity: '1' }
+                },
+            },
+            animation: {
+                sloganReveal: 'sloganReveal 2s linear 1s 1 normal forwards',
+                particleReveal: 'particleReveal 2s linear 1s 1 normal forwards',
+            }
+		}
+	},
+};
+
+export default config;
